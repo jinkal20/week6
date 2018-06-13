3c09e90f5e8136aca77bd8f959fef19804be3d36
Patel <j_patel73637@tss-hr420-dm27.fc.ca>
2018-06-13 14:02:04 -0400

    js update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@
 (function (){
 	console.log("js file loaded");
 	//var
+	var num1=document.querySelector("#input1");
+	var num2=document.querySelector("#input2");
+	var equals= document.querySelector("#trigger");
+	var outcome=document.querySelector("#value");
+	console.log(num1);
+	console.log(num2);
+	console.log(equals);
+	console.log(outcome);
+
+
 	//function
+	function addStuff (argument) {
+		// body...
+		console.log("from addStuff");
+	}
 	//listners
+	equals.addEventListener("click",addStuff,false);
 })();
