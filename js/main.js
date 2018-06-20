61e7d02df6dfca525716041f13737dbeaa88c005
Patel <j_patel73637@tss-hr420-dm29.fc.ca>
2018-06-20 14:10:51 -0400

    js updated

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@
 	var num2=document.querySelector("#input2");
 	var equals= document.querySelector("#trigger");
 	var outcome=document.querySelector("#value");
+	var setPlus= document.querySelector("#plus");
+	var setMinus= document.querySelector("#minus");
 	console.log(num1);
 	console.log(num2);
 	console.log(equals);
 	console.log(outcome);
+	console.log(setPlus);
+	console.log(setMinus);
 
 
 	//function
@@ -21,8 +25,24 @@
 		var total=parseInt(num1.value) + parseInt(num2.value);
 		//console.log(total);
 		outcome.innerHTML=total;
-
+	}
+	function selCal(evt){
+		//console.log("From selCal");
+		/*if (selPlus.innerHTML == "+") {
+			console.log("User clicked +");
+		}
+		else{
+			console.log("User clicked -");
+		}*/
+		//console.log(evt.currentTarget.id);
+		if (evt.currentTarget.id == "plus") {
+			console.log("add");
+		}else{
+			console.log("sub");
+		}
 	}
 	//listners
 	equals.addEventListener("click",addStuff,false);
+	setPlus.addEventListener("click",selCal,false);
+	setMinus.addEventListener("click",selCal,false);
 })();
